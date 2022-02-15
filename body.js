563f959e8db36d99d52d2af1b78c83935a3fa6ac
Aurnab Das <59371873+Aurnab990@users.noreply.github.com>
2022-02-15 21:54:06 +0600

    LED Light script

---
+++ body.js
@@ -0,0 +1,8 @@
+function on(){
+    pic="ON-removebg-preview.png";
+    document.getElementById('img').src=pic;
+}
+function off(){
+    pic="OFF-removebg-preview.png";
+    document.getElementById('img').src=pic;
+}
